8a1703ca6aa5916657cc55df0d6bb5ba5aa1f0f2
keenwon <semanwmj@gmail.com>
2018-12-22 19:54:12 +0800

    添加 env

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,13 @@
-'use strict';
+"use strict"
 
 module.exports = {
-  'extends': 'standard'
-};
+  extends: "standard",
+  env: {
+    browser: true,
+    node: true,
+    commonjs: true,
+    es6: true,
+    mocha: true,
+    jest: true
+  }
+}
